f146174d74f26c3a29c346f2e501f6b38e5973ac
masud rana <masudranaacb@gmail.com>
2022-03-13 08:08:03 -0800

    using setinterval

---
+++ app.js
@@ -0,0 +1,12 @@
+const images=[
+    'img/download.jfif',
+    'img/download (1).jfif',
+    'img/download (2).jfif',
+    'img/download (3).jfif',
+]
+image=0
+
+setInterval((images) => {
+    console.log()
+    image++
+}, 1000);
