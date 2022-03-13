482a4ee19b3a5de9a455b4d4f9ce73011ae67354
masud rana <masudranaacb@gmail.com>
2022-03-13 08:18:50 -0800

    complet slider

--- app.js
+++ app.js
@@ -4,9 +4,12 @@ const images=[
     'img/download (2).jfif',
     'img/download (3).jfif',
 ]
+let showImg=document.getElementById('showImg')
 image=0
-
-setInterval((images) => {
-    console.log()
+setInterval(() => {
+    if(image>=images.length){
+        image=0
+    }
+    showImg.setAttribute('src',images[image])
     image++
 }, 1000);
